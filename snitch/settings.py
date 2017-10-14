f86502f654c211677ab0557bcf9b25d930ea6573
Valder Gallo <valdergallo@gmail.com>
2017-10-14 01:07:45 -0300

    fix static files

--- snitch/settings.py
+++ snitch/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Django Rest Framework config
 REST_FRAMEWORK = {
